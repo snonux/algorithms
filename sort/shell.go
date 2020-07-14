73ca4d0b86036a41b452212702e6aa669888d740
Paul Buetow <paul@buetow.org>
2020-07-14 22:41:35 +0100

    actually add shell sort

---
+++ sort/shell.go
@@ -0,0 +1,32 @@
+package sort
+
+import (
+	"algorithms/ds"
+)
+
+func Shell(a []ds.Comparer) []ds.Comparer {
+	length := len(a)
+	// h-sort the array
+	h := 1
+
+	for h < length/3 {
+		// 1, 4, 13, 40, 121, 364, 1093...
+		h = 3*h + 1
+	}
+
+	for h >= 1 {
+		for i := h; i < length; i++ {
+			for j := i; j >= h; j -= h {
+				if a[j].LowerThan(a[j-h]) {
+					tmp := a[j]
+					a[j] = a[j-h]
+					a[j-h] = tmp
+				}
+			}
+		}
+
+		h /= 3
+	}
+
+	return a
+}
