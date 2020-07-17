82a314d3f211f9da4f0a906ccbe2df279c79e6de
Paul Buetow <paul@buetow.org>
2020-07-17 09:25:59 +0100

    initial shuffle sort

---
+++ sort/shuffle.go
@@ -0,0 +1,24 @@
+package sort
+
+import (
+	"algorithms/ds"
+)
+
+func Shuffle(a ds.ArrayList) ds.ArrayList {
+	length := len(a)
+	for i := 0; i < length; i++ {
+		min := i
+		for j := i + 1; j < length; j++ {
+			if a[min].Higher(a[j]) {
+				min = j
+			}
+		}
+		if min == i {
+			continue
+		}
+		tmp := a[i]
+		a[i] = a[min]
+		a[min] = tmp
+	}
+	return a
+}
